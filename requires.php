86024504ecec776d093e50a5273001ce8985d247
Ezequiel Cortés <ecortes@izelnetworks.com>
2018-09-19 17:50:43 -0300

    - Se agregan las nuevas dependencias al archivo de requires

--- requires.php
+++ requires.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * Archivo de requires
+ *
+ * En este archivo deben agregarse las dependencias del proyecto.
+ *
+ * @todo En el futuro implementar autoloading de clases. Ver: http://php.net/manual/es/language.oop5.autoload.php.
+ */
 
-require_once "forms/Form.php";
-require_once "forms/UserRegisterForm.php";
+/* Config */
+require_once "config.php";
+
+/* Forms */
+require_once "Forms/Form.php";
+require_once "Forms/UserRegisterForm.php";
+
+/* Entities */
+require_once "Entities/User.php";

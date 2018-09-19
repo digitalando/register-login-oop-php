088d3f26de668821636cf4df58262b2391449ddd
Ezequiel Cortés <ecortes@izelnetworks.com>
2018-09-19 17:49:07 -0300

    - Se termina la entidad de usuario.

--- Entities/User.php
+++ Entities/User.php
@@ -5,14 +5,19 @@ namespace AFS\Entitites;
 /**
  * User Entity
  *
- * This class provides the user entity
+ * Esta clase provee la entidad de usuarios.
  */
 class User {
 
   /**
+   * @var int
+   */
+  private $id;
+
+  /**
    * @var string
    */
-  private $fullName;
+  private $fullname;
 
   /**
    * @var string
@@ -27,11 +32,6 @@ class User {
   /**
    * @var string
    */
-  private $passwordConfirm;
-
-  /**
-   * @var string
-   */
   private $country;
 
   /**
@@ -39,21 +39,105 @@ class User {
    */
   private $image;
 
-  public function __construct($post, $files) {
-    // Si el post llega vacío dejamos los campos vacíos
-    $this->fullName = $userFullName;
-    $this->email = $userEmail;
-    $this->password = $userPassword;
-    $this->passwordConfirm = $userRePassword;
-    $this->country = $userCountry;
-    $this->image = $files['userAvatar'];
+  public function __construct($fullname, $email, $password, $country, $image) {
+    $this->fullname = $fullname;
+    $this->email = $this->setEmail($email);
+    $this->password = $this->setPassword($password);
+    $this->country = $country;
+    $this->image = $image;
   }
 
- /**
-  * @return string
-  */
-  public function getFullName() {
-    return $this->fullName;
+  /**
+   * Setea el parámetro id
+   *
+   * @param int
+   */
+  public function setImage($id) {
+    $this->id = $id;
+  }
+
+
+  /**
+   * Setea el parámetro fullname
+   *
+   * @param string
+   */
+  public function setFullname($fullname) {
+    $this->fullname = $fullname
+  }
+
+  /**
+   * Setea el parámetro email
+   *
+   * La función también quitará los espacios adicionales y transformará el email a minúsculas.
+   *
+   * @param string
+   */
+  public function setEmail($email) {
+    $this->email = trim(strtolower($email);
+  }
+
+  /**
+   * Setea el parámetro password
+   *
+   * La función utiliza el método de password_hash
+   *
+   * @param string $password
+   */
+  public function setPassword($password) {
+    /* Si el password ya fue hasheado lo guardamos como está */
+    if (passwordIsHashed($password) == false) {
+      $password = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    $this->password = $password;
+  }
+
+  /**
+   * Chequea si un password ya fue hasheado anteriormente
+   *
+   * Fuentes:
+   * - http://php.net/manual/en/function.password-get-info.php
+   *
+   * @param string $password
+   * @return boolean
+   */
+  private function passwordIsHashed($password) {
+    $info = password_get_info($password);
+    
+    return $info['algo'] != 0;
+  }
+
+  /**
+   * Setea el parámetro fullname
+   *
+   * @param string
+   */
+  public function setCountry($country) {
+    $this->country = $country;
+  }
+
+  /**
+   * Setea el parámetro fullname
+   *
+   * @param string
+   */
+  public function setImage($image) {
+    $this->image = $image;
+  }
+
+  /**
+   * @return int
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * @return string
+   */
+  public function getfullname() {
+    return $this->fullname;
   }
 
   /**
@@ -78,7 +162,7 @@ class User {
   }
 
   /**
-   * @return array
+   * @return string
    */
   public function getImage() {
     return $this->image;
